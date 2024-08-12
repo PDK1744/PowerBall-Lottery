fdcbb3ffb3391ad58b819684032238a843aacb81
Kobe Bonner <119374028+PDK1744@users.noreply.github.com>
2024-08-12 15:04:38 +0000

    Intial Commit

---
+++ powerball.py
@@ -0,0 +1,13 @@
+import random
+
+print('''Powerball Lottery
+
+Each powerball lottery ticket cost $2. The jackpot for this game is $1.586 billion! The odds are 1 in 292,201,338.
+
+This simulation gives you thr thrill of playing without wasting real money''')
+
+# Player enters the first five numbers, 1 to 69:
+while true:
+  print('Enter 5 different numbers from 1-69, with spaces between each number.')
+  print('For example: 5 17 23 42 60')
+  
